9fc4edf79aed046bdbf1ba6206f207ff38bd8c8d
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-24 07:08:16 +0200

    unstable: etcd range tests.

---
+++ src/Orleans.Persistence.Morstead.Etcd.Tests/EtcdClientTests.cs
@@ -0,0 +1,30 @@
+﻿using dotnet_etcd;
+using Newtonsoft.Json;
+using Orleans.Reminders.Morstead.Etcd.Storage;
+using Xunit;
+
+namespace Orleans.Persistence.Morstead.Etcd.Tests
+{
+    public class EtcdClientTests
+    {
+        [Fact]
+        public async void CanDeserializeKvsFromRange()
+        {
+            JsonConvert.SerializeObject(new MorsteadEtcdBasedReminderEntry()
+            {
+                ETag = "ETag"
+            });
+
+            var client = new EtcdClient("http://localhost:2379");
+            await client.PutAsync("key/1", JsonConvert.SerializeObject(new MorsteadEtcdBasedReminderEntry()
+            {
+                ETag = "ETag"
+            }));
+            var s =  await client.GetRangeValAsync("key/*");
+            foreach (var i in s)
+            {
+
+            }
+        }
+    }
+}
